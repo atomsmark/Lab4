4913ea9227da139001ca945f56da47e8398c1ce2
Melisa730 <117914333+Melisa730@users.noreply.github.com>
2023-12-18 14:15:55 +0000

    test

--- Anteckningar.hs
+++ Anteckningar.hs
@@ -20,3 +20,18 @@ readEvalPrint = do      -- 5A (5)
     case parse expr s' of
         Just (ans,"") -> print (eval ans)
         _             -> putStrLn "Invalid Expression" -}
+
+{-
+* Lecture 5a (2), min 5.40, parsing case of, analyserar siffror från en lista med string
+* Parse a single digit lecture 5(C) min 2
+
+--example  Lecture 5a (2)
+num :: ParserFun Integer
+num s= case span isDigit s of
+    (d:ds, rest) -> Just (read (d:ds), rest)
+    _            -> Nothing
+addition0
+multiplication0
+calculation0 s = case addition0 s of
+                      Nothing ->
+-}
