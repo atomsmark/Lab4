736dbe840f3d71232093b34011879cad54385d0d
atomsmark <agnes.tomsmark@gmail.com>
2023-12-20 15:12:31 +0000

    commit

--- Expr.hs
+++ Expr.hs
@@ -1,21 +1,25 @@
 import Test.QuickCheck
 import GHC.Natural (naturalFromInteger)
-import Prelude
+import Prelude 
+import DynFlags (IntegerLibrary(IntegerSimple))
+import Parsing
+import RegAlloc.Graph.Stats (addSRM)
+import Test.QuickCheck.Text (number)
 
 
 --A---------------------------------------------
 data BinOp = Add | Mul
-    deriving Eq
+    deriving (Eq, Show)
 
 data TrigOp = Sin | Cos
-    deriving Eq
+    deriving (Eq, Show)
 
 data Expr
     = Num Double
     | Var
     | Bin BinOp Expr Expr
     | Trig TrigOp Expr
-    deriving Eq
+    deriving (Eq, Show)
 
 
 x :: Expr
@@ -40,8 +44,8 @@ size (Trig op n) = 1 + (size n)
 --A-------------------------------------------------
 
 --B-------------------------------------------------
-instance Show Expr where
-    show = showExpr
+--instance Show Expr where
+--    show = showExpr
 
 showExpr :: Expr -> String
 showExpr (Num n)     = show n
@@ -60,19 +64,126 @@ showTrig n              = showExpr n
 
 --C-------------------------------------------------
 eval :: Expr -> Double -> Double
-eval (
+eval (Num n) _       = n
+eval (Var) value     = value
+eval (Bin Add n m) x = eval n x + eval m x 
+eval (Bin Mul n m) x = eval n x * eval m x 
+eval (Trig Sin e) x  = Prelude.sin (eval e x)
+eval (Trig Cos e) x  = Prelude.cos (eval e x)
 
 
-eval :: Expr -> Double
-eval (Num n)     = n
-eval (Add e1 e2) = eval e1 + eval e2
-eval (Mul e1 e2) = eval e1 * eval e2
-eval (Sin e)     = sin (eval e)
-eval (Cos e)     = cos (eval e)
+--D-------------------------------------------------
+-- | Runs the parser on the given string 
+--  to return maybe a thing and a string
+--parse :: Parser a -> String -> Maybe(a,String)
+--parse (P f ) s = f s
 
 
-vars :: Expr -> [String]
-vars (Num n)      = []
-vars (Var s)      = [s]
-vars (Add e1 e2)  = vars e1 ++ vars e2
-vars (Mul e1 e2)  = vars e1 ++ vars e2
+
+readExpr :: String -> Maybe Expr
+readExpr input = case parse expr input of
+                 Just (a, rest) -> Just a
+                 _              -> Nothing
+
+expr, term, factor :: Parser Expr
+expr = foldl1 (Bin Add) <$> chain term (char '+')
+term = foldl1 (Bin Mul) <$> chain factor (char '*')
+factor = Num <$> readsP
+      <|> char '(' *> expr <* char ')'
+      <|> char 'x' *> return Var
+      <|> char 's' char 'i' char 'n' *> (Trig Sin) expr
+
+
+--prop_readExpr :: Expr -> Bool
+--prop_readExpr input = readExpr (showExpr input) == input
+
+
+{-assoc :: Expr -> Expr
+assoc (Add (Add e1 e2) e3) = assoc (Add e1 (Add e2 e3))
+assoc (Add e1          e2) = Add (assoc e1) (assoc e2)
+assoc (Sub e1 e2)          = Sub (assoc e1) (assoc e2)
+assoc (Lit n)              = Lit n -}
+                      
+
+------------------------------------------------------------------------
+--Parsea OCH kalkylera:
+{-type ParserFun a = String -> Maybe (a,String)
+
+num :: ParserFun Integer
+num s = case span isDigit s of
+    (d:ds, rest) -> Just (read (d:ds), rest)
+    _            -> Nothing
+
+addition0 :: ParserFun Integer
+addition0 s = case num s of   --Olika fall för vad num s blir (Alltså en Just (ngt,ngt))
+      Just (n, '+':r) -> case num r of       --Då nästa tecken som inte är en siffra är ett +
+                              Just (m, r') -> Just (n+m, r')        --Adderar n och m (alla siffror fram till nästa tecken som inte är en siffra)
+                              _            -> Nothing
+      _               -> Nothing
+
+multiplication0 :: ParserFun Integer
+multiplication0 s = case num s of   --Olika fall för vad num s blir (Alltså en Just (ngt,ngt))
+      Just (n, '*':r) -> case num r of       --Då nästa tecken som inte är en siffra är ett *
+                              Just (m, r') -> Just (n*m, r')        --multiplicerar n med m (alla siffror fram till nästa tecken som inte är en siffra)
+                              _            -> Nothing
+      _               -> Nothing
+
+calculation0 s = case addition0 s of
+                      Nothing -> multiplication0 s
+                      ok      -> ok-}
+
+--------------------------------------------------------------------------
+{-expr, term, factor :: Parser Expr
+
+expr = foldl1 Add <$> chain term (char '+')
+term = foldl1 Mul <$> chain factor (char '*')
+factor = Num <$> number <|> char '(' *> expr <* char ')'
+
+
+expr'  = do
+    t <- term
+    ts <- zeroOrMore (do char '+'; term)
+    return $ foldl Add t ts
+
+term'  = do
+    t <- factor
+    ts <- zeroOrMore (do char '*'; factor)
+    return $ foldl Mul t ts
+
+
+factor' = Num <$> number
+     <|> do char '('
+            e <- expr
+            char ')'
+            return e
+
+chain item sep = do
+    i <- item
+    is <- zeroOrMore (sep *> item)
+    return (i:is)-}
+
+--E--------------------------------------------------------------------
+
+
+
+
+--F--------------------------------------------------------------------
+
+
+
+
+--G--------------------------------------------------------------------
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
